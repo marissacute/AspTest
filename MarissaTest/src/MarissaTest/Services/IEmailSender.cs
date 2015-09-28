30cd665aa5bfbed7d922b9803ac6914034cbff60
Marissa Hoek <leofoxus@gmail.com>
2015-09-28 19:18:21 +0200

    Rename to MarissaTest

---
+++ MarissaTest/src/MarissaTest/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarissaTest.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
